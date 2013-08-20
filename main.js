ffe74c0e43975fa2d202db770b85e21681ef7d2c
Chris Kelner <chris.kelner@gmail.com>
2013-08-20 16:50:38 -0400

    config file instead of hardcoded.  Minor tweaks to main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 var AWS = require('aws-sdk');
-AWS.config.update({
-  accessKeyId: "REPLACEME",
-  secretAccessKey: "REPLACEME",
-  region: 'us-east-1'
-});
+AWS.config.loadFromPath('./config.json');
+AWS.config.update({region: 'us-east-1'});
 
 var ec2 = new AWS.EC2();
 
 ec2.describeVolumes(null, function(err, data) {
-  
-  var count=0;
-  for(var i=0; i<data.Volumes.length; i++) {
-    console.log("Volume: ", data.Volumes[i].VolumeId);
-    count++;
+  if(err) {
+    console.log("error: " + err)
   }
-  console.log("manual count:"+count.toString());
-  console.log("data array count:"+data.Volumes.length.toString());
+  if(data) {
+    var count=0;
+    for(var i=0; i<data.Volumes.length; i++) {
+      console.log("Volume: ", data.Volumes[i].VolumeId);
+      count++;
+    }
+    console.log("manual count: "+count.toString());
+    console.log("data array count: "+data.Volumes.length.toString());
+  } else {
+    console.log("no data...")
+  }
 });
