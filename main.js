04fd3e3ac0c5dff19ae73fd9b384eebee151fcfa
Chris Kelner <ckelner@users.noreply.github.com>
2016-01-07 21:39:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ var params = {
   Delimiter: ',',
   EncodingType: 'url'
 };
+var s3 = new AWS.S3();
 s3.listObjects(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
